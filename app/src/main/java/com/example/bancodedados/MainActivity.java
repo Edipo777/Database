da075428ae94f93618da1084423d15bd1db3eb12
Edipo777 <80066073+Edipo777@users.noreply.github.com>
2021-11-03 09:17:05 -0300

    Teste GIT. Feito Layout simples.

---
+++ app/src/main/java/com/example/bancodedados/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.bancodedados;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
